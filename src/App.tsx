f4c309471e63c98bf1112f5e5e5e7d6b7f66e18d
Danila Zotov <71969881+DanilaZotov@users.noreply.github.com>
2022-11-23 13:38:47 +0500

    Первый коммит файлов

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import '/css/style.css'
+
+function App() {
+  return (
+    <>
+    <header className='header'>
+      <div className="wrapper">
+        <div className="header__wrapper">
+          <div className="header__logo">
+            <a href="/" className="header__logo-link">
+              <img src="./logo.svg" alt="" className='header__logo-pic' />
+            </a>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <main className="main">
+      <section className="intro">
+
+      </section>
+    </main>
+    </>
+  )
+}
+
+export default App
