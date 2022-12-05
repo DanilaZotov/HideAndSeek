770af8e052e9c83e409cc9a2591e4d929803fe23
Danila Zotov <71969881+DanilaZotov@users.noreply.github.com>
2022-12-05 19:01:36 +0500

    навбар

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import '/css/style.css'
+import * as React from 'react';
 
 function App() {
   return (
@@ -11,13 +12,32 @@ function App() {
               <img src="./logo.svg" alt="" className='header__logo-pic' />
             </a>
           </div>
+          <div className='search-bar'>
+            <div className='search-icon'>
+              <img src="./microdog.svg" alt="" className='search__pic' />
+	          </div>
+            <input type="text" placeholder="Гипоаллергенные собаки"/>
+            <button type="submit"><a>Найти</a></button>
+          </div>
+          <a className='create-animal-link'>
+            <img src="./createanimal.svg" alt="" className='' />
+          </a>
+          <a className='articles-link-pic'>
+            <img src="./articles.svg" alt="" className='' />
+          </a>
+          <a className='messages-link-pic'>
+            <img src="./message.svg" alt="" className='' />
+          </a>
+          <a className='profile-link-pic'>
+            <img src="./profile.svg" alt="" className='' />
+          </a>
         </div>
       </div>
     </header>
 
     <main className="main">
       <section className="intro">
-
+        <div className='waveheader'></div>
       </section>
     </main>
     </>
